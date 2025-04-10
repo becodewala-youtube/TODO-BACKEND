80eb14c5c90476ecb5c424cd36fbe4e8ee246ced
becodewala-youtube <kvikash74673@gmail.com>
2025-04-10 15:06:57 +0530

    cors

--- server.js
+++ server.js
@@ -18,29 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-/* const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  'http://localhost:5173',
-  'https://todo-mern-becodewala.vercel.app'
-];
+
 
 app.use(cors({
   origin: (origin, callback) => {
-    // Allow requests with no origin (server-to-server or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-})); */
-
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5173','http://localhost:5174', 'https://todo-mern-becodewala.vercel.app']; 
+    const allowedOrigins = ['http://localhost:5173','http://localhost:5174', 'https://todo-frontend-becodewala.vercel.app']; 
     // Add all allowed origins here
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);  // Allow the request
