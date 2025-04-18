6dce527835358f878cd0e66341217c996cd01db6
Vikash Kumar <83962116+becodewala-youtube@users.noreply.github.com>
2025-04-19 00:00:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,15 +13,22 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:5173','http://localhost:5174', 'https://todo-frontend-becodewala.vercel.app']; // Add all allowed origins here
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);  // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS'));  // Reject the request
+    }
+  },
+  credentials: true,  // Allow cookies to be sent
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Middleware
-app.options('*', cors({  
-  origin: 'https://todo-frontend-becodewala.vercel.app',  
-  credentials: true,  
-})); 
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
